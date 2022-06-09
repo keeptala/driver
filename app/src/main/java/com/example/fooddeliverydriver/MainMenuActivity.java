9d064d1cf5f4b6ec38901c8a8dc96bc9a98dcacf
Brian Omondi <omondibrian392@gmail.com>
2022-06-09 15:14:19 -0400

    project commit

---
+++ app/src/main/java/com/example/fooddeliverydriver/MainMenuActivity.java
@@ -0,0 +1,16 @@
+package com.example.fooddeliverydriver;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
